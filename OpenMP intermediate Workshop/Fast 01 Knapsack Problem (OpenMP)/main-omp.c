9cdb7588aa719e7a028624a93ddf12294395a90b
sytjason <jasont1020508@gmail.com>
2021-06-19 18:32:18 +0800

    big update

---
+++ OpenMP intermediate Workshop/Fast 01 Knapsack Problem (OpenMP)/main-omp.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#define MAXN 10001
+#define MAXM 1000001
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+struct object{
+	int weight;
+	int value;
+};
+
+void print(struct object items[], int N){
+	for(int i = 0; i < N; i++){
+		printf("%d %d\n", items[i].weight, items[i].value);
+	}
+}
+
+int main(){
+	int N, M;
+	struct object items[MAXN];
+	int dp[2][MAXM];
+	memset(dp, 0, sizeof(dp));
+
+	scanf("%d %d", &N, &M);
+	for(int i = 0; i < N; i++){
+		int weight, value;
+		scanf("%d %d", &weight, &value);
+		items[i].weight = weight, items[i].value = value;
+	}
+
+#pragma omp parallel
+	for(int i = 0; i < N; i++){
+		int w = items[i].weight, v = items[i].value;
+		if(i % 2){ // assign new dp from dp[1] to dp[0]
+			#pragma omp for
+			for(int j = 0; j < w; j++) dp[0][j] = dp[1][j];
+			#pragma omp for
+			for(int j = w; j <= M; j++) dp[0][j] = max(dp[1][j-w] + v, dp[1][j]);
+		}else{ // assign new dp from dp[0] to dp[1]
+			#pragma omp for
+			for(int j = 0; j < w; j++) dp[1][j] = dp[0][j];
+			#pragma omp for
+			for(int j = w; j <= M; j++) dp[1][j] = max(dp[0][j-w] + v, dp[0][j]);
+		}
+	}
+
+	printf("%d\n", (N-1) % 2 ? dp[0][M] : dp[1][M]);
+
+	return 0;
+}
---
+++ OpenMP intermediate Workshop/Fast N-Queens Basic (OpenMP)/main-seq.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXN 20
+
+void printBoard(char board[MAXN][MAXN], int n){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++)
+			printf("%c", board[i][j]);
+		printf("\n");
+	}
+}
+
+int check(char board[MAXN][MAXN], int placement[MAXN], int row, int col){
+	if(board[row][col] == '*')return -1;
+	for(int i = 0; i < row; i++){
+		if(placement[i] == col || row - i == abs(col - placement[i]))
+			return -1;
+	}
+	return 1;
+}
+
+int do_queen(char board[MAXN][MAXN], int placement[MAXN], int row, int n){
+	if(row == n)return 1;
+	int sum = 0;
+	for(int i = 0; i < n; i++){
+		if(check(board, placement, row, i) == 1){
+			placement[row] = i;
+			sum += do_queen(board, placement, row+1, n);
+		}
+	}
+	return sum;
+}
+
+int main(){
+	int n;
+	int placement[MAXN];
+	int c = 1;
+	char board[MAXN][MAXN];
+
+	while(scanf("%d ", &n) == 1){
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				scanf("%c ", &board[i][j]);
+			}
+		}
+
+		int sum = 0;
+		for(int i = 0; i < n; i++){
+			if(board[0][i] == '*')continue;
+			placement[0] = i;
+			sum += do_queen(board, placement, 1, n);
+		}
+
+		printf("Case %d: %d\n",c++, sum);
+	}
+	return 0;
+}
